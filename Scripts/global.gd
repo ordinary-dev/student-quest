992628cb21283e8952caf475a9653100597997aa
IvanR <mcraw@pm.me>
2020-02-03 22:01:49 +0500

    Start from scratch: new audio player, global script and menu scene

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,6 +1,35 @@
 extends Node
 
-var date
+var health = 0
+var type = 2
+var potatos = 1
+var file_name = "user://save_game.dat"
 
-func _ready():
-	 date = "26 июня 2020"
+func gen_dict():
+	var save_dict = {
+		"health":health,
+		"type":type,
+		"potatos":potatos
+	}
+	return save_dict
+
+func restore(content):
+	health = content["health"]
+	type = content["type"]
+	potatos = content["potatos"]
+
+func read_file(path=file_name):
+	var fl = File.new()
+	fl.open(path, File.READ)
+	var content = parse_json(fl.get_line())
+	fl.close()
+	restore(content)
+	
+func write_file(path=file_name):
+	var fl = File.new()
+	fl.open(path, File.WRITE)
+	var node = gen_dict()
+	fl.store_line(to_json(node))
+	fl.close()
+	
+	
