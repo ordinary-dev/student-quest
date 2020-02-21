c11a16690a4d29283e6b0f5f4eabd222014186ea
IvanR <mcraw@pm.me>
2020-02-22 01:21:53 +0500

    Fix pause button when joystick is being used

--- Scripts/common.gd
+++ Scripts/common.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
 
-func _process(delta):
+# Нажатие ESC
+func _process(_delta) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_node("Position2D/Pause").switch()
 
 
-func _ready():
-	# UI parent obj
+# Инициализация интерфейса
+func _ready() -> void:
+	# Родитель для UI
 	var pos = Position2D.new()
 	pos.name = "Position2D"
 	add_child(pos)
+	# Передача координат игрока
 	get_node("Body/Character/Camera2D/RemoteTransform2D").remote_path = "/root/Node2D/Position2D"
-	
+	# Появление паузы и джойстика на мобильных платформах
 	get_node("/root/pause_init").init()
