41a7c57bca536e39864441b479c4e21da3ea57ea
IvanR <mcraw@pm.me>
2020-02-22 16:44:29 +0500

    Control dialog using keyboard

--- Scripts/dialog.gd
+++ Scripts/dialog.gd
@@ -2,23 +2,33 @@ extends Node
 
 var dialog_scene : = load("res://Scenes/dialog.tscn")
 var effect
+var s_content : Dictionary
+var s_key : String
 
 func hide_dialog():
 	get_node("/root/Node2D/Position2D").remove_child(get_node("/root/Node2D/Position2D/dialog"))
+	set_process(false)
 
-func show_next(content : Dictionary, key : String) -> void:
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		if s_key != "-1":
+			show_next()
+		else:
+			hide_dialog()
+
+func show_next() -> void:
 	var name : = get_node("/root/Node2D/Position2D/dialog/Dialog/Main/Margin/HBox/VBox/Name")
 	var text : = get_node("/root/Node2D/Position2D/dialog/Dialog/Main/Margin/HBox/VBox/Text")
 	var btn : = get_node("/root/Node2D/Position2D/dialog/Dialog/Main/Margin/HBox/Button")
 	text.percent_visible = 0
-	name.text = content[key]["name"]
-	text.text = content[key]["text"]
-	if content[key].has("next"):
-		key = content[key]["next"]
+	name.text = s_content[s_key]["name"]
+	text.text = s_content[s_key]["text"]
+	if s_content[s_key].has("next"):
+		s_key = s_content[s_key]["next"]
 		btn.disconnect("pressed", self, "show_next")
-		btn.connect("pressed", self, "show_next", [content, key])
+		btn.connect("pressed", self, "show_next", [s_key])
 	else:
-		key = "-1"
+		s_key = "-1"
 		btn.disconnect("pressed", self, "show_next")
 		btn.connect("pressed", self, "hide_dialog")
 	effect.interpolate_property(text, "percent_visible", 0, 1, len(text.text) / 30.0, Tween.TRANS_LINEAR)
@@ -39,6 +49,12 @@ func show_dialog(num : int) -> void:
 		fl.close()
 		# Найти Tween
 		effect = get_node("/root/Node2D/Position2D/dialog/Tween")
+		# Активировать отслеживание кнопки
+		set_process(true)
 		# Показать диалог
-		show_next(content, "0")
+		s_content = content
+		s_key = "0"
+		show_next()
 		
+func _ready():
+	set_process(false)
