94ac110dbce3e0ea6bd513f5225c01e268dc7700
IvanR <mcraw@pm.me>
2020-02-22 20:41:08 +0500

    New character animation

--- Scripts/navigation/Character.gd
+++ Scripts/navigation/Character.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 export (int) var speed = 100
 var velocity = Vector2()
 onready var character = $Character
+var distance_passed = 0
+export (int) var change = 10
+var type = 0
 
 func lock():
 	set_physics_process(false)
@@ -17,10 +20,30 @@ func get_input():
 	if Input.is_action_pressed('ui_left'):
 		velocity.x -= 1
 	if Input.is_action_pressed('ui_down'):
-		character.frame = 0
+		if type != 0:
+			distance_passed = 0
+			type = 0
+			character.frame = 0
+		distance_passed += 1
+		if distance_passed >= change:
+			if character.frame == 0:
+				character.frame = 2
+			else:
+				character.frame = 0
+			distance_passed = 0
 		velocity.y += 1
 	if Input.is_action_pressed('ui_up'):
-		character.frame = 1
+		if type != 1:
+			distance_passed = 0
+			type = 1
+			character.frame = 1
+		distance_passed += 1
+		if distance_passed >= change:
+			if character.frame == 1:
+				character.frame = 3
+			else:
+				character.frame = 1
+			distance_passed = 0
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
