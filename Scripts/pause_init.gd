130a71bd2129a38b86a2b0fa4385fcbd07f9affa
Ivan <mcraw@pm.me>
2020-03-04 19:58:47 +0500

    Improve some scripts

--- Scripts/pause_init.gd
+++ Scripts/pause_init.gd
@@ -1,19 +1,35 @@
 extends Node
 
+# Сцены паузы и управления
 export (String, FILE, "*.tscn") var pause_scene = "res://Scenes/templates/pause.tscn"
 export (String, FILE, "*.tscn") var contr_scene = "res://Scenes/templates/Controls.tscn"
+# Принудительно включить джойстик
 var SHOW_CONTROLS = false
 
-func init():
+
+# Добавить кнопку паузы
+func add_pause() -> void:
 	var sc = load(pause_scene).instance()
 	sc.name = "Pause"
 	get_node("/root/ui").add_child(sc)
-	if OS.get_name() == "Android" || SHOW_CONTROLS:
-		sc = load(contr_scene).instance()
-		sc.name = "Controls"
-		get_node("/root/ui").add_child(sc)
 
-func disable():
+
+# Добавить управление
+func add_controls() -> void:
+	var sc = load(contr_scene).instance()
+	sc.name = "Controls"
+	get_node("/root/ui").add_child(sc)
+
+
+# Создать интерфейс
+func init() -> void:
+	add_pause()
+	if OS.get_name() == "Android" || SHOW_CONTROLS:
+		add_controls()
+
+
+# Удалить интерфейс
+func disable() -> void:
 	if has_node("/root/ui/Pause"):
 		get_node("/root/ui").remove_child(get_node("/root/ui/Pause"))
 	if has_node("/root/ui/Controls"):
