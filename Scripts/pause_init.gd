50019be23f64ebed5fbc295fe11c733abf31ee7f
Ivan <mcraw@pm.me>
2020-04-07 22:28:01 +0500

    Change trees and add time on top

--- Scripts/pause_init.gd
+++ Scripts/pause_init.gd
@@ -1,38 +1,58 @@
 extends Node
 
 # Сцены паузы и управления
-export (String, FILE, "*.tscn") var pause_scene = "res://Scenes/ui/Pause.tscn"
-export (String, FILE, "*.tscn") var contr_scene = "res://Scenes/ui/Controls.tscn"
+const pause_scene = "res://Scenes/ui/Pause.tscn"
+const contr_scene = "res://Scenes/ui/Controls.tscn"
+const time_scene = "res://Scenes/ui/Time.tscn"
 # Принудительно включить джойстик
 const SHOW_CONTROLS = false
+const ui_base = "/root/ui/CanvasLayer"
 
 
 # Добавить кнопку паузы
 func add_pause() -> void:
 	var sc = load(pause_scene).instance()
 	sc.name = "Pause"
-	get_node("/root/ui/CanvasLayer").add_child(sc)
+	get_node(ui_base).add_child(sc)
 
 
 # Добавить управление
 func add_controls() -> void:
 	var sc = load(contr_scene).instance()
 	sc.name = "Controls"
-	get_node("/root/ui/CanvasLayer").add_child(sc)
+	get_node(ui_base).add_child(sc)
+
+
+# Добавить интерфейс времени
+func show_time(time) -> void:
+	if !has_node(ui_base + "/Time"):
+		var time_obj = load(time_scene).instance()
+		time_obj.name = "Time"
+		get_node(ui_base).add_child(time_obj)
+		get_node(ui_base + "/Time").set_time(time)
+		get_node(ui_base + "/Time").start()
+
+
+# Скрыть время
+func hide_time() -> void:
+	if has_node(ui_base + "/Time"):
+		get_node(ui_base + "/Time").stop()
+		get_node(ui_base).remove_child(get_node(ui_base + "/Time"))
 
 
 # Создать интерфейс
 func enable_ui() -> void:
-	if !has_node("/root/ui/CanvasLayer/Pause"):
+	if !has_node(ui_base + "/Pause"):
 		add_pause()
 	if OS.get_name() == "Android" or SHOW_CONTROLS:
-		if !has_node("/root/ui/CanvasLayer/Controls"):
+		if !has_node(ui_base + "/Controls"):
 			add_controls()
 
 
 # Удалить интерфейс
 func disable_ui() -> void:
-	if has_node("/root/ui/CanvasLayer/Pause"):
-		get_node("/root/ui/CanvasLayer").remove_child(get_node("/root/ui/CanvasLayer/Pause"))
-	if has_node("/root/ui/CanvasLayer/Controls"):
-		get_node("/root/ui/CanvasLayer").remove_child(get_node("/root/ui/CanvasLayer/Controls"))
+	if has_node(ui_base + "/Pause"):
+		get_node(ui_base).remove_child(get_node(ui_base + "/Pause"))
+	if has_node(ui_base + "/Controls"):
+		get_node(ui_base).remove_child(get_node(ui_base + "/Controls"))
+
