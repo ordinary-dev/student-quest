342460114e227dea50b6a585a178737dc72e9a3f
Ivan <mcraw@pm.me>
2020-07-04 21:17:27 +0500

    Update scenes of the first chapter

--- Scripts/Blackout.gd
+++ Scripts/Blackout.gd
@@ -4,22 +4,35 @@ extends Node2D
 
 export (String, FILE, "*.tscn") var hidden_room 
 
+export (String, FILE, "*.json") var dialog_1
+export (String, FILE, "*.json") var dialog_2
+export (String, FILE, "*.json") var dialog_3
+var ind = 1
+
+onready var trigger_1 = $"GuardDialog-Trigger_Area2D_0"
+onready var trigger_2 = $"Stairs-DialogTrigger-Trigger_Area2D_0"
+
 onready var color_rect = $CanvasLayer/Blackout_ColorRect
 onready var tween = $Tween
-onready var guard = $"GuardDialog;DialogTrigger-Trigger_Area2D"
 onready var map = $"Math-map-Template"
 
 const c1 := Color("#00080808")
 const c2 := Color("#ee080808")
 
 
-func adventure_begins():
-	SCENES.load_scene(hidden_room)
+func dialog() -> void:
+	if ind == 1:
+		DIALOG.show_dialog(dialog_1)
+		ind = 2
+	elif ind == 2:
+		DIALOG.show_dialog(dialog_2)
+		ind = 3
+	else:
+		DIALOG.show_dialog(dialog_3)
 
 
 func turn_lights_off():
 	yield(get_tree().create_timer(2), "timeout")
-	TEMP.save("LIGHTS_OFF", true)
 	tween.interpolate_property(
 		color_rect, "color",
 		c1, c2, 1,
@@ -27,23 +40,16 @@ func turn_lights_off():
 	tween.start()
 
 
-func turn_lights_off_now():
-	color_rect.color = c2
-
-
 func turn_lights_on():
-	TEMP.remove_key("LIGHTS_OFF")
 	tween.interpolate_property(
 		color_rect, "color",
 		c2, c1, 1,
 		Tween.TRANS_BOUNCE, Tween.EASE_IN_OUT)
 	tween.start()
 	map.block_turnstile = false
-	guard.used = true
+	remove_child(trigger_1)
+	remove_child(trigger_2)
 
 
 func _ready():
-	if TEMP.is_saved("LIGHTS_OFF"):
-		turn_lights_off_now()
-	else:
-		turn_lights_off()
+	turn_lights_off()
