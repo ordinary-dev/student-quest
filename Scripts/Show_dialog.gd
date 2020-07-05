e800c1a4053b50972ea76adeeb7b533464a180ba
Ivan <mcraw@pm.me>
2020-07-05 21:38:10 +0500

    Add new scenes at the end of the first day

--- Scripts/Show_dialog.gd
+++ Scripts/Show_dialog.gd
@@ -2,6 +2,8 @@ extends Node
 
 
 export (String, FILE, "*.json") var dialog_path
+export (bool) var enable_delay = false
+export (float) var delay = 2.0
 export (bool) var load_scene = false
 export (String, FILE, "*.tscn") var scene_path
 
@@ -11,6 +13,8 @@ func load_next_scene() -> void:
 
 
 func _ready() -> void:
+	if enable_delay:
+		yield(get_tree().create_timer(delay), "timeout")
 	if load_scene:
 		DIALOG.show_dialog(dialog_path, get_path(), "load_next_scene")
 	else:
