ae5eba8f356934d6f89782be150649fd4e6dca67
Ivan <mcraw@pm.me>
2020-07-05 19:54:03 +0500

    Add travel scenes

---
+++ Scripts/Show_dialog.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+export (String, FILE, "*.json") var dialog_path
+export (bool) var load_scene = false
+export (String, FILE, "*.tscn") var scene_path
+
+
+func load_next_scene() -> void:
+	SCENES.load_scene(scene_path)
+
+
+func _ready() -> void:
+	if load_scene:
+		DIALOG.show_dialog(dialog_path, get_path(), "load_next_scene")
+	else:
+		DIALOG.show_dialog(dialog_path)
