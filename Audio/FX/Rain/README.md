e6d42175ed842af3b6f671f57fcca63f75b6172f
Ivan <mcraw@pm.me>
2020-08-01 15:34:47 +0500

    Change menu structure and background animation

---
+++ Audio/FX/Rain/README.md
@@ -0,0 +1,2 @@
+# License information
+* [Rain, Moderate, C](https://freesound.org/people/InspectorJ/sounds/401275/) by [InspectorJ](http://www.jshaw.co.uk/) is licensed under [CC BY 3.0](http://creativecommons.org/licenses/by/3.0/) 
---
+++ Scripts/Scenes/Menu/Menu.gd
@@ -0,0 +1,66 @@
+# Menu interface script
+
+# Copyright (c) 2020 PixelTrain
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+extends Control
+
+# Path to the scene with new game
+export (String, FILE, "*.tscn") var new_game_scene
+
+export (NodePath) var game_loading_scene_path
+export (NodePath) var settings_scene_path
+export (NodePath) var confirmation_scene_path
+
+export (NodePath) var new_game_btn
+
+var game_loading_scene
+var settings_scene
+var confirmation_scene
+
+
+# Create new game
+func _new_game() -> void:
+	FX.btn_click()
+	GLOBAL.write_settings()
+	SCENES.load_scene(new_game_scene)
+
+
+# Show saved games
+func _load_game() -> void:
+	FX.btn_click()
+	game_loading_scene.visible = !game_loading_scene.visible
+	settings_scene.visible = false
+	confirmation_scene.visible = false
+
+
+# Show or hide settings
+func _settings() -> void:
+	FX.btn_click()
+	settings_scene.visible = !settings_scene.visible
+	game_loading_scene.visible = false
+	confirmation_scene.visible = false
+
+
+# Confirmation
+func _quit() -> void:
+	FX.btn_click()
+	confirmation_scene.visible = !confirmation_scene.visible
+	game_loading_scene.visible = false
+	settings_scene.visible = false
+
+
+func _ready() -> void:
+	# Get nodes
+	game_loading_scene = get_node(game_loading_scene_path)
+	settings_scene = get_node(settings_scene_path)
+	confirmation_scene = get_node(confirmation_scene_path)
+	# Disable scenes
+	game_loading_scene.visible = false
+	settings_scene.visible = false
+	confirmation_scene.visible = false
+	# Focus
+	get_node(new_game_btn).grab_focus()
