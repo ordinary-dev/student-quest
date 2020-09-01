b01cfbc8eb5d038f7d4e9466e998a7c8e6ac07ed
Ivan <mcraw@pm.me>
2020-09-01 12:52:42 +0500

    Update info and credits

--- Scripts/Scenes/Menu/Settings/Info.gd
+++ Scripts/Scenes/Menu/Settings/Info.gd
@@ -5,6 +5,8 @@
 
 extends Node
 
+export (String, FILE, "*.tscn") var credits_scene
+
 
 func open_site(link : String) -> void:
 	var err = OS.shell_open(link)
@@ -16,13 +18,5 @@ func open_store() -> void:
 	open_site("https://pixeltrain.itch.io/student-quest")
 
 
-func freesound() -> void:
-	open_site("https://freesound.org/")
-
-
-func zapsplat() -> void:
-	open_site("https://www.zapsplat.com/")
-
-
-func outgoing_hikikomori() -> void:
-	open_site("https://www.youtube.com/channel/UC3yRCpfOE2zfTQsOHCH8wZw")
+func open_credits() -> void:
+	SCENES.load_scene(credits_scene)
