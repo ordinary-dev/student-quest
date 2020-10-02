15e91b846c5a305b37b67d1cbe0c1549b0cd719e
Ivan <mcraw@pm.me>
2020-10-02 17:05:41 +0500

    Add source files for music and a script for converting to .ogg

---
+++ sh/music_converter.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Settings
+BITRATE="256k"
+CODEC="libvorbis"
+EXT=".ogg"
+
+# Go to music directory
+SAVED_PWD=$PWD
+SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
+cd $SCRIPT_DIR
+cd ../Audio/Music
+
+
+for i in ".wav" ".mp3"
+do
+    find . -name "*$i" | while read line; do
+	NEWFILE=${line/$i/$EXT}
+	echo -ne "\r\033[KConverting $line..."
+	ffmpeg -nostdin -y -i "$line" -ac 2 -ar 44100 -b:a $BITRATE -c:a $CODEC "$NEWFILE" &> /dev/null
+    done
+done
+
+echo -e "\r\033[KDone"
+
+# Return to original directory
+cd $SAVED_PWD
